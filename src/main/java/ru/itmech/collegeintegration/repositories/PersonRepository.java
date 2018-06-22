a8896a11ec1ecdc54a448d69fa7ecb15ad7c1c98
aivanov <na131@mail.ru>
2018-06-22 12:10:26 +0300

    comments for method are added

--- src/main/java/ru/itmech/collegeintegration/repositories/PersonRepository.java
+++ src/main/java/ru/itmech/collegeintegration/repositories/PersonRepository.java
@@ -8,9 +8,21 @@ import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
 
+    /**
+     * Метод для поиска в базе данных объекта по номеру СНИЛСА
+     *
+     * @param numeral номер СНИЛСА
+     * @return объект из базы данных с указаннм номером СНИЛСА
+     */
     @Query("select p from Person as p join p.docs as d where d.numeral = ?1 ")
     List<Person> findBySnils(String numeral);
 
+    /**
+     * Метод для поиска в базе данных объекта по полному имени (ФИО)
+     *
+     * @param fullname полное имя (ФИО)
+     * @return  объект из базы данных с указанным ФИО
+     */
     @Query("select p from Person as p where lower(p.fullName) like ?1 ")
     List<Person> findByFullName(String fullname);
 }
