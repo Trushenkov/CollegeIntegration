7b0ce75381c5d7d9d58c633bc2d667e94bf9592b
aivanov <na131@mail.ru>
2018-06-22 12:07:42 +0300

    added comments for methods

--- src/main/java/ru/itmech/collegeintegration/controllers/PersonController.java
+++ src/main/java/ru/itmech/collegeintegration/controllers/PersonController.java
@@ -3,6 +3,7 @@ package ru.itmech.collegeintegration.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itmech.collegeintegration.domain.Person;
 import ru.itmech.collegeintegration.repositories.PersonRepository;
@@ -19,12 +20,25 @@ public class PersonController {
         this.repository = repository;
     }
 
-    @RequestMapping(value = "findbyfio/{lastName} {firstName} {middleName}")
+    /**
+     * Метод для обработки get-запроса по адресу localhost:8080/findbyfio/{lastName} {firstName} {middleName}
+     *
+     * @param lastName Фамилия
+     * @param firstName Имя
+     * @param middleName Отчество
+     * @return найденный из базы данных по ФИО объект класса Person
+     */
+    @RequestMapping(value = "findbyfio/{lastName} {firstName} {middleName}", method = RequestMethod.GET)
     public List<Person> requestByFullname(@PathVariable("lastName") String lastName, @PathVariable("firstName") String firstName, @PathVariable("middleName") String middleName) {
         return repository.findByFullName(lastName.toLowerCase() + " " + firstName.toLowerCase() + " " + middleName.toLowerCase());
     }
 
-    @RequestMapping(value = "findbysnils/{snils}")
+    /**
+     * Метод для обработки get-запроса по адресу localhost:8080/findbysnils/{snils}
+     * @param snils номер снилса
+     * @return найденный из базы данных по номеру СНИЛСА объект класса Person
+     */
+    @RequestMapping(value = "findbysnils/{snils}", method = RequestMethod.GET)
     public List<Person> requestBySnils(@PathVariable("snils") String snils) {
         return repository.findBySnils(snils);
     }
